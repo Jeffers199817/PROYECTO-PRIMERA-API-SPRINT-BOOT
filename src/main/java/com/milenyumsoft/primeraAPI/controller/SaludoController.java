b396916a12c0996cfda47e55f6952904d8e22fb7
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2024-11-14 10:44:09 -0500

    Final de mi primera Api

---
+++ src/main/java/com/milenyumsoft/primeraAPI/controller/SaludoController.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.milenyumsoft.primeraAPI.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author JEFFERSON
+ */
+
+@RestController
+@RequestMapping("/apisaludos")
+public class SaludoController {
+    
+    
+    @GetMapping("/hola")
+    public String holaMundo(){ 
+        return " Hola mundo ds";
+    }
+    
+    
+    @GetMapping("/holanombre/{nombre}/{edad}")
+    public String holMundoNombre(@PathVariable String nombre, @PathVariable int edad){
+        return " Hola mundo!" + nombre + " Tu edad es: " + edad;
+    }
+}
